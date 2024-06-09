0f30bb9d21b09e2bbe7d2945a4e49f9cb283279f
Vachhani Rahul <vachanirahul54@gmail.com>
2024-06-09 17:10:00 +0530

    first commit

---
+++ Task 1/script.js
@@ -0,0 +1,16 @@
+// script.js
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message. We will get back to you shortly.');
+    this.reset();
+});
